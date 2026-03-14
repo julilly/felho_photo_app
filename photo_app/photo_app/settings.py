93c5749d907edd6d3fa0741f4649f87733bbb207
julilly <100561566+julilly@users.noreply.github.com>
2026-03-14 17:47:28 +0100

    db name corrected

--- photo_app/photo_app/settings.py
+++ photo_app/photo_app/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = "photo_app.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'photos_app_db',
+        'NAME': 'photos-app-db',
         'USER': 'db_user',
         'PASSWORD': os.getenv('DB_PASSWORD'), 
     }
